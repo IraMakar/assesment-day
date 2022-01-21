9726acbb41a77cdccac22dce94a2f3727cfd0732
makarova16 <77056095+IraMakar@users.noreply.github.com>
2022-01-21 15:45:50 +0200

    fixes for cors

--- clients/src/http/peopleAPI.js
+++ clients/src/http/peopleAPI.js
@@ -1,37 +1,41 @@
 import { $host } from "./index";
 
 export const createPeople = async (people) => {
-  const { data } = await $host.post("api/people", people);
+  const { data } = await $host.post("api/people", people, {
+    headers: {"Access-Control-Allow-Origin": "*"}
+  });
   return data;
 };
 
 export const fetchPeople = async (page, limit = 4) => {
   // const { data } = await $host.get("api/people?limit=4");
-  const { data } = await $host.get("api/people", {params: {
+  const { data } = await $host.get("api/people", {headers: {"Access-Control-Allow-Origin": "*"}, params: {
     page, limit
   }});
   return data;
 };
 
 export const fetchOnePeople = async (id) => {
-  const { data } = await $host.get("api/people/" + id);
+  const { data } = await $host.get("api/people/" + id, { headers: {"Access-Control-Allow-Origin": "*"} });
   return data;
 };
 
 export const updatePeople = async (body = {}) => {
-  const { data } = await $host.put("api/people/update", body);
+  const { data } = await $host.put("api/people/update", body, {headers: {"Access-Control-Allow-Origin": "*"}});
   return data;
 };
 
 export const updateOnePeople = async ({ id, sum_mark }) => {
   const { data } = await $host.put("api/people/update/" + id, {
     sum_mark,
-  });
+  }, { headers: {"Access-Control-Allow-Origin": "*"} });
   return data;
 };
 
 export const deleteOnePeople = async (id) => {
-  const { data } = await $host.delete("api/people/delete/" + id);
+  const { data } = await $host.delete("api/people/delete/" + id, {
+    headers: {"Access-Control-Allow-Origin": "*"}
+  });
   return data;
 };
 // export const deleteOnePeople = async ({ id }) => {
